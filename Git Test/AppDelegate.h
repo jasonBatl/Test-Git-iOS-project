5127cc0c8f48aa336af2ef10db404f72fe56becc
Bronson, Jason <Jason.Bronson@carters.com>
2014-10-04 18:02:52 -0400

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Bronson, Jason on 10/4/14.
+//  Copyright (c) 2014 Bronson, Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Bronson, Jason on 10/4/14.
+//  Copyright (c) 2014 Bronson, Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
